f0f1994c86105a4cbf118c8230a337ee790b5c9b
Haro Pierre <pierre.haro@ensta-paristech.fr>
2019-04-09 15:39:53 +0200

    Amelioration minimax

--- Checkers/minimax.py
+++ Checkers/minimax.py
@@ -1,15 +1,12 @@
 def minimax(state, depth, maximize):
     if(depth==0 or state.get_children()==[]):
-        return state.evaluate()    
-
+        return state.evaluate()  
+    value=float("-inf")  
     if maximize==True:
-        value=float("-inf")
         for i in state.get_children():
             value=max(value,minimax(i,depth-1,False))
-        return value
     else:
-        value=float("+inf")
         for i in state.get_children():
-            value=min(value,minimax(i,depth-1,True))
-        return value
+            value=min(-value,minimax(i,depth-1,True))
+    return value
 
