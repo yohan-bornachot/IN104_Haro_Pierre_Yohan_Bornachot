1a9486f1f1723701d13f6fefd4bf7cc3be0abef5
Bornachot Yohan <yohan.bornachot@ensta-parsitech.fr>
2019-04-16 16:52:19 +0200

    ajout du fichier match entre deux IA avec elagage alpha beta

---
+++ Checkers/match_AI_alphabeta_vs_AI_alphabeta.py
@@ -0,0 +1,20 @@
+import IN104_simulateur as simu
+from MinimaxBrain import MinimaxBrain
+rules={}
+rules['menCaptureBackward']=False
+rules['kingsCanFly']=True
+rules['menMustStop']=False
+rules['noCaptureMax']=16
+
+brain1 = MinimaxBrain()
+brain2 = MinimaxBrain()
+player_time = 50 #the player will have 30s to play
+ai_time = 20 #the AI will have 10 sec to play
+game = simu.Game(brain1, ai_time, brain2, ai_time, rules=rules)
+game.displayLevel = 1   # this prints the board after each move
+game.runGame()
+print(game.pdn) #print the summary of the game. 
+
+result=open("score.txt","a")
+result.write(game.pdn)
+result.close()
