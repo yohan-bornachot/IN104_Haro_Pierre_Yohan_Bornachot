041098d2ac7eeb6a92b14b19f52bd9e3d398f7f7
Haro Pierre <pierre.haro@ensta-paristech.fr>
2019-05-07 16:57:45 +0200

    Nouvelle fonction evaluate en construction

---
+++ Checkers/evaluation_improving.py
@@ -0,0 +1,18 @@
+import IN104_simulateur as simu
+
+def evaluate_improving(state):
+    Cell = simu.Cell
+    boardState = state.boardState
+    score = 0
+    cell = boardState.cells
+    for k in range(0,32):
+        [r,c] = boardState.indexToRC(k)
+        if cell[k] is Cell.w:
+            score =+ r
+        elif cell[k] is Cell.b:
+            score -= r
+        elif cell[k] is Cell.B:
+            score -= 10
+        elif cell[k] is Cell.W:
+            score += 10
+    return score
