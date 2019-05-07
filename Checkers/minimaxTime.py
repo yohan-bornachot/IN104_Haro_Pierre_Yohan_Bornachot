d458b804e00d1abaab13e4afd24d3f1dbb0bc9cd
Bornachot Yohan <yohan.bornachot@ensta-parsitech.fr>
2019-05-07 16:51:51 +0200

    ajout du fichier minimaxTime.py, qui crée une IA utilisant minimax_time (prise en compte du temps)

---
+++ Checkers/minimaxTime.py
@@ -0,0 +1,25 @@
+import IN104_simulateur as simu
+import time
+from evaluation_bis import evaluate
+from minimax_time import minimax
+
+
+class MinimaxTime:
+
+    def __init__(self, config=None, rules=None):
+        self.name = 'AI' # set your AI name here
+
+    def play(self, gameState, timeLimit):
+        possibleStates = gameState.findNextStates()
+        value=float("-inf")
+        dico_state={}
+        next_state = gameState
+        for state in possibleStates:
+            new_value = minimax(state,0.1,True,simu.GameState.findNextStates,evaluate,dico_state)
+            if new_value >= value:
+                value = new_value
+                next_state = state
+        return next_state
+    
+    def __str__(self):
+        return self.name
